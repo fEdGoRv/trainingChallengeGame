1423f7ed9d62af05a4c8d7380b4b2df68f7e95b6
Federico <vilatte612@gmail.com>
2024-04-01 11:33:57 -0300

    gameboard actulitation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import Header from './Components/Pure/Header.jsx'
 import { useState } from 'react';
 import Log from './Components/Pure/log.jsx';
 
+let inicialGameBoard = [
+  [
+      'Click Me', 'Click Me', 'Click Me'
+  ],
+  [
+      'Click Me', 'Click Me', 'Click Me'
+  ],
+  [
+      'Click Me', 'Click Me', 'Click Me'
+  ]
+];
+
+
 function deriveActivePlayer(gameTurns){
   let currentPlayer = 'X';
 
@@ -20,11 +33,19 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  const gameBoard = inicialGameBoard;
+    for(const turn of gameTurns){
+        let {square, player} = turn;
+        const {row, col} = square;
+        
+        gameBoard[row][col] = player;    
+    }
+
   function handleSquareSymbol(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
 
       const currentPlayer = deriveActivePlayer(prevTurns);
-      
+
       const updatedTurns = [{
         square: { row: rowIndex, col: colIndex },
         player: currentPlayer 
@@ -36,7 +57,7 @@ function App() {
 return (
   <div className="App">
     <Header activePlayer={activePlayer} />
-    <GameBoard handleSquareSymbol={handleSquareSymbol} turns={gameTurns} />
+    <GameBoard handleSquareSymbol={handleSquareSymbol} board={gameBoard} />
     <Log turns={gameTurns} />
   </div>
 );
--- src/Components/Pure/GameBoard.jsx
+++ src/Components/Pure/GameBoard.jsx
@@ -2,33 +2,14 @@
 import Button from './Button';
 
 
-export default function GameBoard({handleSquareSymbol, turns}) {
-    console.log(turns)
+export default function GameBoard({handleSquareSymbol, board}) {
 
-        let inicialGameBoard = [
-            [
-                'Click Me', 'Click Me', 'Click Me'
-            ],
-            [
-                'Click Me', 'Click Me', 'Click Me'
-            ],
-            [
-                'Click Me', 'Click Me', 'Click Me'
-            ]
-        ];
-
+        
     const orangeButton = "bg-amber-500 text-white px-10 py-10 rounded text-xl text-center cursor-pointer font-bold item-center m-14 border-solid border-2"  
 
-    const gameBoard = inicialGameBoard;
-    for(const turn of turns){
-        let {square, player} = turn;
-        const {row, col} = square;
-        
-        gameBoard[row][col] = player;    
-    }
     
     return <ol className='flex border-solid border-4 border-red-500 mx-12 my-12 rounded-md bg-neutral-800'>
-        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {board.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => <li key={colIndex}><Button className={orangeButton} player={playerSymbol} onClick={() => handleSquareSymbol(rowIndex, colIndex)}>{playerSymbol}</Button>;
                 </li>)}
